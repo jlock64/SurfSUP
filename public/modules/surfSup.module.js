4db5d2e5f385e411e5ccdaf9bd28a418e3c97a7a
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-04-06 14:37:55 -0400

    added jquery as dependency

--- public/modules/surfSup.module.js
+++ public/modules/surfSup.module.js
@@ -4,6 +4,10 @@ angular
   .module('surfSup', ['$ngRoute'])
   .config(function($ngProvider) {
     $routeProvider
+      .when('/login', {
+        template: "<h1> create is working! </h1>",
+        controller: "loginController"
+      });
       .when('/create', {
         template: "<h1> create is working! </h1>",
         controller: "createController"
