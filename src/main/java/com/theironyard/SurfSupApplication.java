42c0a8b7e6c4350c8d841be1ea358ad27c20e5d7
drtonton <tkfoster26@gmail.com>
2016-04-05 21:39:33 -0400

    initial import

---
+++ src/main/java/com/theironyard/SurfSupApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfSupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfSupApplication.class, args);
+	}
+}
