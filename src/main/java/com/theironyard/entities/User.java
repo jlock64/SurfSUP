e355f21b17313e59e18a7f8085048ec2f01b13aa
drtonton <tkfoster26@gmail.com>
2016-04-06 13:21:41 -0400

    added user class. missing two fields (defaultLocation and profileImage). will update later

--- src/main/java/com/theironyard/entities/User.java
+++ src/main/java/com/theironyard/entities/User.java
@@ -1,7 +1,35 @@
 package com.theironyard.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by noellemachin on 4/6/16.
  */
 public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String firstName;
+
+    @Column(nullable = false)
+    String lastName;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false, unique = true)
+    String phone;
+
+//    @Column(nullable = false)
+//    // defaultLocation.. unsure how to do this right now
+
+    @Column(nullable = false, unique = true)
+    String userName;
+
+    @Column(nullable = false)
+    String password;
 }
