2fc482a3b9cc612a6768e0bca50e61092b117811
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-04-07 09:47:03 -0400

    updated controller

--- public/controllers/controller.login.js
+++ public/controllers/controller.login.js
@@ -5,7 +5,12 @@ angular
     $scope.loginObj = {};
     $scope.login = function() {
       console.log('login object:', $scope.loginObj);
-      LoginService.loginUser($scope.loginObj);
+      LoginService.loginUser($scope.loginObj).success(function (res) {
+        console.log('we can redirect here if so', res);
+      })
+      .error(function (err) {
+        console.log('doh');
+      })
     };
 
   }) // end of LoginController
