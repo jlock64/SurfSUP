cc771d1577ad33905840f51190da53e170812059
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-04-07 12:54:32 -0400

    refactored login controller

--- public/bundle.js
+++ public/bundle.js
@@ -43,20 +43,23 @@ angular
   .module('surfSup')
   .controller('LoginController', function($scope, LoginService) {
 
+    $scope.login = login;
+    $scope.logout = logout;
     $scope.loginObj = {};
 
-    $scope.login = function() {
+    function login() {
       console.log('login object:', $scope.loginObj);
       LoginService.loginUser($scope.loginObj).success(function (res) {
         console.log('we can redirect here if so', res);
       })
       .error(function (err) {
         console.log('doh');
+        $('#usernameAlert').html('<div class="alert alert-danger" role="alert"><strong>Oh snap!</strong> You have entered the wrong password! Try again.</div>');
       })
     };
 
 
-    $scope.logout = function() {
+    function logout() {
       LoginService.logoutUser();
       console.log('logging out');
     }
