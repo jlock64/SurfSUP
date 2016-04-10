3cff9333ec25e1272d95fc2a3c3af64905d3bc95
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-04-09 22:00:12 -0400

    added edit and delete to sessions page

--- public/controllers/sessionActivityController.js
+++ public/controllers/sessionActivityController.js
@@ -1,25 +1,30 @@
-angular
-  .module('surfSup')
-  .controller('SessionActivityController', function ($scope, SessionService, CacheEngine) {
-    if (CacheEngine.get('seshActivity')){
-      var cache = CacheEngine.get('seshActivity');
-      $scope.seshActivity = cache;
-      console.log('cache is working!');
-    }
-    else {
-        SessionService.getSession()
-        .then(function(data) {
-          CacheEngine.put('seshActivity', data);
-          $scope.seshActivity = data;
-          window.glow = data;
-          console.log('data pulling is working!');
-        });
-    }
-
-    $scope.deleteSession = function (id) {
-      console.log('this is id', id);
-      SessionService.deleteSession(id);
-    }
-
-
-  });
+// angular
+//   .module('surfSup')
+//   .controller('SessionActivityController', function ($scope, SessionService, CacheEngine) {
+//
+//     if (CacheEngine.get('seshActivity')){
+//       var cache = CacheEngine.get('seshActivity');
+//       $scope.seshActivity = cache;
+//       console.log('cache is working!');
+//     }
+//     else {
+//         SessionService.getSession()
+//         .then(function(data) {
+//           CacheEngine.put('seshActivity', data);
+//           $scope.seshActivity = data;
+//           window.glow = data;
+//           console.log('data pulling is working!');
+//         ;});
+//     }
+//
+//     $scope.deleteSession = function (id) {
+//       console.log('this is id', id);
+//       SessionService.deleteSession(id);
+//     };
+//
+//     $scope.editSession = function (editedSession) {
+//       SessionService.editSession(editedSession);
+//     };
+// 
+//
+//   }); // end of SessionActivityController
