14c1881b1a718be3f51766287c8a1253d015ff30
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-04-13 14:34:36 -0400

    idk

--- public/bundle.js
+++ public/bundle.js
@@ -177,12 +177,12 @@ angular
     }
 
     // addSesh update
-    $scope.$on('session:added', function() {
-      SessionService.getSession()
-        .success(function(sessions) {
-          $scope.seshActivity = sessions;
-        })
-    })
+    // $scope.$on('session:added', function() {
+    //   SessionService.getSession()
+    //     .success(function(sessions) {
+    //       $scope.seshActivity = sessions;
+    //     })
+    // })
 
     // deleteSession
     function deleteSession(id) {
