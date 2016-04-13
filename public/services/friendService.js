318d8dd16e35b3374ad1bc41d5129965f7c89ccd
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-04-12 20:57:39 -0400

    working on modal

--- public/services/friendService.js
+++ public/services/friendService.js
@@ -4,25 +4,21 @@ angular
 
     var searchFriendsUrl = '/user';
     function findFriends() {
-      console.log('finding friends', searchFriendsUrl);
       return $http.get(searchFriendsUrl);
     }
 
     var friendInvitationUrl = '/friend';
     function friendInvitation(username) {
-      console.log('inviting friends', friendInvitationUrl);
       return $http.post(friendInvitationUrl, username);
     }
 
     var requestAmtUrl = '/requestAmt';
     function requests() {
-      console.log('friend requests', requestAmtUrl);
       return $http.get(requestAmtUrl);
     }
 
     var requestListUrl = '/requests';
     function requestList() {
-      console.log('request list:', requestListUrl);
       return $http.get(requestListUrl);
     }
 
